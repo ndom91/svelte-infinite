69d5bde83a2818868fd79a627c03ff4dad9e9002
ndom91 <yo@ndo.dev>
2024-03-06 13:51:18 +0100

    fix: use snippet instead of slot in demo app layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { type Snippet } from "svelte"
   import "../global.css"
+
+  const { children } = $props<{ children: Snippet }>()
 </script>
 
-<slot />
+{@render children()}
